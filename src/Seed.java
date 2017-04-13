7119e98df8433eb754121767b6163b8016055dc2
max <max.mesirow@gmail.com>
2017-04-13 19:52:23 -0400

    my seed

--- src/Seed.java
+++ src/Seed.java
@@ -7,6 +7,8 @@ public class Seed
     final int seedShift = 5000;
     final int bigPrimeOne = 4759;
     final int bigPrimeTwo = 3719;
+    final int townSizeX = 30;
+    final int townSizeY = 30;
     int seedGen;
     public Seed()
     {
@@ -17,11 +19,19 @@ public class Seed
     public tileType getTile(int x, int y)
     {
 
-        x/=40;
-        y/=40;
+        x/=40;//pixels
+        y/=40;//pixels
 
-        int randomGened = (bigPrimeOne*x-1)%seedGen + 7*(bigPrimeTwo*y-23)%(seedGen+3*(x/5)+7*(y/5));
-        
+
+        int randomGened = (bigPrimeOne*x-1)%seedGen + (bigPrimeTwo*y-1)%seedGen + 7*(bigPrimeTwo*y-23)%(seedGen+3*(x/5)+7*(y/5));
+        int randomTowns = (bigPrimeOne*(x/townSizeX)-1)%seedGen+ (bigPrimeTwo*(y/townSizeY)-1)%seedGen + 7*(bigPrimeTwo*(y/townSizeY)-23)%(seedGen+3*((x/townSizeX)/5)+7*((y/townSizeY)/5));
+
+        //calculate towns
+        if(randomTowns%15==0) //the block is in a town
+        {
+            return tileType.shortGrass;//right now it deforests but will place town here
+        }
+
         //sudo random aspect
         int rando = randomGened%randomDeviationAspect;
         if (rando == 0)
