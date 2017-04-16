87760e92a8f0a4ad240dd24c9d95055151f41114
max <max.mesirow@gmail.com>
2017-04-15 22:25:03 -0400

    city collisions

---
+++ src/cityColisionCalculator.java
@@ -0,0 +1,24 @@
+public class cityCollisionCalculator
+{
+    public static ArrayList<Rectangle> getCityColisions(int tileX,int tileY,tileType tile)
+    {
+        switch(tile)
+        {
+            case viridianCity:
+                return viridianCity(tileX,tileY);
+
+        }
+        
+    }
+
+    public static ArrayList<Rectangle> virirdianCity(int tileX, int tileY)
+    {
+        relativeX = (tileX+Seed.xShift)%Seed.townSizeX
+        relativeY = (tileY+Seed.yShift)%Seed.townSizeY
+
+        ArrayList<Rectangle> ret = new ArrayList<Rectangle>();
+        case
+
+
+    }
+}
